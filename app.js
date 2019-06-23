7d1531a155b05c15c5f6171790f73205a491e0df
Josue Valencia <josue.valencia@domo.ec>
2019-06-23 12:08:51 +0200

    Handling de errores

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,5 +20,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);  
+  res.status(error.status || 500);
+  const data = {};
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    res.status(400);
+    data.errors = {}
+    Object.keys(error.errors)
+      .forEach(field => data.errors[field] = error.errors[field].message)
+  } else if (error instanceof mongoose.Error.CastError) {
+    res.status(404);
+    error.message = 'Resource not found';
+  }
+
+  data.message = error.message
+  res.json(data);
+})
 
 module.exports = app;
